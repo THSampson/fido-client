682614311f47ee6ffe3dc01500352a2dc37e839f
Taylor Sampson <thsampsn@gmail.com>
2020-10-12 11:00:44 -0400

    first push

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,11 +1,15 @@
 let APIURL = '';
 
-switch (window.location.hostname) {
+switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-        APIURL = 'httpL//localhost:5432'
+        APIURL = 'http://localhost:3000';
         break;
         case 'ts-finding-fido.herokuapp.com':
             APIURL = 'https://finding-fido-server.herokuapp.com'
+            break;
+        default:
+        alert("url error");
+        break;
 }
 
 export default APIURL;
