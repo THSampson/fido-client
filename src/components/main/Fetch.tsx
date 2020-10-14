a248f8b5d93e9a6d969628889ac366d284405f75
Taylor Sampson <thsampsn@gmail.com>
2020-10-14 11:18:12 -0400

    finished fetch

--- src/components/main/Fetch.tsx
+++ src/components/main/Fetch.tsx
@@ -1,96 +1,116 @@
 import React from "react";
-import {Radio, TextField, FormGroup, FormControl, FormLabel, FormControlLabel, RadioGroup, Button, Paper} from '@material-ui/core';
-import APIURL from '../../helpers/environment';
-import FetchResults from '../main/FetchResults';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import IconButton from '@material-ui/core/IconButton';
-
-interface Data {
-  name: string;
-  age: string;
- gender: string;
-  photos: string;
-  url: string
-  }
+import {
+  Radio,
+  TextField,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  Button,
+  Paper,
+} from "@material-ui/core";
+import APIURL from "../../helpers/environment";
+import FetchResults from "../main/FetchResults";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import IconButton from "@material-ui/core/IconButton";
 
 type IProps = {
-    sessionToken: string, 
-    }
+  sessionToken: string;
+};
 
 interface IState {
-data: Data,
-location: string,
-type: string,
-expanded: boolean,
-
-
+  data: [];
+  location: string;
+  type: string;
+  expanded: boolean;
+   // name: string;
+  // age: string;
+  // gender: string;
+  // photos: any;
+  // url: string;
 }
 
 class Fetch extends React.Component<IProps, IState> {
-constructor(props: IProps) {
-super(props) 
-this.state = {
-location: '',
-type: '',
-expanded: false,
-data: { name: "", age: "", gender: '', photos: '', url: "" },
-}
-}
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      location: "",
+      type: "",
+      expanded: false,
+      data: [],
+      // name: "",
+      // age: "",
+      // gender: "",
+      // photos: {},
+      // url: "",
+    };
+  }
 
+  fetchResults = () => {
+    let url: string = `${APIURL}/animals`;
+    let animalObject = {
+      location: this.state.location,
+      type: this.state.type,
+    };
 
-fetchResults = () => {
-let url: string = `${APIURL}/animals`;
-let animalObject = {
-  location: this.state.location,
-  type: this.state.type,
-};
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(animalObject),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: this.props.sessionToken,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+           this.setState({
+          data: data.animals,
+         });
+      })
+      .catch((err) => console.log(err));
+  };
 
-fetch(url, {
-  method: "POST",
-  body: JSON.stringify(animalObject),
-  headers: new Headers({
-      'Content-Type': 'application/json',
-      "Authorization": this.props.sessionToken
-  })
-})
-  .then((res) => res.json())
-  .then((data) => {
-   console.log(data.animals[0].name) 
-    this.setState({
-    data: data.animals
-  })
-  
-})
- .catch((err) => console.log(err));
-}
-
-
-handleSubmit= (event:any) => {
-  this.fetchResults();
-  event.preventDefault();
-}
-
+  handleSubmit = (event: any) => {
+    this.fetchResults();
+    event.preventDefault();
+  };
 
   render() {
     return (
       <div className="App">
-    <FormControl onSubmit={(event) => this.handleSubmit(event)}>
-      <FormLabel>Type</FormLabel>
-      <RadioGroup aria-label="type" value={this.state.type} onChange={(event) => this.setState({type: event.target.value})} name="type1" row>
-      <FormControlLabel value="dog" control={<Radio />} label="Dog" />
-       <FormControlLabel value="cat" control={<Radio />} label="Cat" />
-       </RadioGroup>
-       <br />
-       <TextField id="zipcode" variant="outlined" label="zipcode" value={this.state.location} onChange={(event) => this.setState({location: event.target.value})} />
-    <Button variant="outlined" type="submit" onClick={this.handleSubmit} >Fetch!</Button>
-    </FormControl>
+        <FormControl onSubmit={(event) => this.handleSubmit(event)}>
+          <FormLabel>Type</FormLabel>
+          <RadioGroup
+            aria-label="type"
+            value={this.state.type}
+            onChange={(event) => this.setState({ type: event.target.value })}
+            name="type1"
+            row
+          >
+            <FormControlLabel value="dog" control={<Radio />} label="Dog" />
+            <FormControlLabel value="cat" control={<Radio />} label="Cat" />
+          </RadioGroup>
+          <br />
+          <TextField
+            id="zipcode"
+            variant="outlined"
+            label="zipcode"
+            value={this.state.location}
+            onChange={(event) =>
+              this.setState({ location: event.target.value })
+            }
+          />
+          <Button variant="outlined" type="submit" onClick={this.handleSubmit}>
+            Fetch!
+          </Button>
+        </FormControl>
 
-{
-  // <FetchResults data={this.state.data}/>
-}
-
-   </div>
-    )
+        {
+          <FetchResults data={this.state.data}/>
+        }
+      </div>
+    );
   }
 }
 export default Fetch;
--- src/components/main/FetchResults.tsx
+++ src/components/main/FetchResults.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Image from '../../assets/no-image.jpg';
+import styled from 'styled-components';
 
+const MyResults = styled.div`
+text-align: center;
+padding-bottom: 30px;
+`
+const MyImg = styled.img`
+right-margin: auto;
+left-margin: auto
+`
 
 interface IState {
 name: string,
 age: string,
 gender: string,
 describe: string,
-photos: string,
+photos: any,
 url: string,
+medium: string,
 }
 
 type IProps = {
-data: any,
+data: [],
 
 }
 
@@ -22,16 +34,17 @@ const FetchResults = (props: IProps) => {
 return(
     <div>
         {props.data.map((result: IState) => {
-          return (<div>
+          return (<MyResults>
                  <h2>{result.name}</h2>
                 <h2>{result.gender}</h2>
                 <h2> {result.age}</h2>
-                <img src={result.photos[1]}/>
-                <p>{result.url}</p>
+                {result.photos.length >= 1 ? <MyImg src={result.photos[0].medium} alt="Animals" /> : <MyImg src={Image} height="100" alt="no image"/>}
+                <br />
+                <Button variant="contained" color="primary" target="_blank" rel="noopener" href={result.url}>Adoption Site</Button>
                 <IconButton aria-label="add to favorites">
                 <FavoriteIcon />
                 </IconButton>
-                </div>
+                </MyResults>
             )
         })
         }
